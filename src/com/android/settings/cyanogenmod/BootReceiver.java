86a54560d5b419f2e15e68b48a1d4323b8c4eb94
Shaun <shaunsibu@gmail.com>
2014-09-18 09:22:20 -0600

    Re-Sync with BeanStalk.
    Change-Id: I9bc9b9ec944efef0404b220fdd456ae9f8b574a8

--- src/com/android/settings/cyanogenmod/BootReceiver.java
+++ src/com/android/settings/cyanogenmod/BootReceiver.java
@@ -2,8 +2,12 @@
 package com.android.settings.cyanogenmod;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.provider.Settings;
 
 import com.android.settings.DisplaySettings;
 import com.android.settings.R;
@@ -12,12 +16,28 @@ import com.android.settings.hardware.DisplayColor;
 import com.android.settings.hardware.DisplayGamma;
 import com.android.settings.hardware.VibratorIntensity;
 import com.android.settings.location.LocationSettings;
+import com.android.settings.beanstalk.service.QuietHoursController;
 
 public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context ctx, Intent intent) {
 
+	ContentResolver resolver = ctx.getContentResolver();
+
+ 	if (Settings.System.getInt(resolver,
+ 		Settings.System.QUIET_HOURS_REQUIRE_CHARGING, 0) != 0) {
+ 	    IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+ 	    Intent batteryStatus = ctx.registerReceiver(null, ifilter);
+ 	    final int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+ 	    final boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+ 	    	status == BatteryManager.BATTERY_STATUS_FULL;
+
+ 	    Settings.System.putInt(resolver,
+ 		Settings.System.QUIET_HOURS_REQUIRE_CHARGING, isCharging ? 2 : 1);
+ }
+
+	QuietHoursController.getInstance(ctx).scheduleService();
         /* Restore hardware tunable values */
         DisplayColor.restore(ctx);
         DisplayGamma.restore(ctx);
