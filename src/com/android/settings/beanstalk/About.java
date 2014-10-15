3b39c2884809b0af93c1427ec7947352e52e31ad
Shaun <shaunsibu@gmail.com>
2014-10-15 17:15:35 -0600

    Point it all towards TeamBliss.
    Change-Id: Ia91f8b9b1e6c5bcbe95186b7861d480ba6d6fc17

--- src/com/android/settings/beanstalk/About.java
+++ src/com/android/settings/beanstalk/About.java
@@ -62,7 +62,7 @@ public class About extends SettingsPreferenceFragment {
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         if (preference == mSiteUrl) {
-            launchUrl("http://www.scotthartbti.com");
+            launchUrl("http://www.teambliss.x10host.com");
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
