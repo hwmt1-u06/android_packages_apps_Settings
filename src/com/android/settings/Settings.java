2f0936065e1f44bf53bc4f4d552979ee3617fc97
Shaun <shaunsibu@gmail.com>
2014-10-23 13:23:11 -0600

    Building fix
    Change-Id: I94b784cb38551ed14da83c5a28531133c8359ae1

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -239,6 +239,11 @@ public class Settings extends PreferenceActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // We only want to inflate the search menu item in the top-level activity
+        if (getClass() != Settings.class) {
+            return false;
+        }
+
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.settings_search, menu);
         mSearchItem = menu.findItem(R.id.action_search);
@@ -270,6 +275,7 @@ public class Settings extends PreferenceActivity
         mSearchBar.setLayoutParams(layoutParams);
         mSearchBar.setHint(R.string.settings_search_autocompleteview_hint);
         mSearchBar.setThreshold(1);
+        mSearchBar.setSingleLine(true);
         mSearchBar.setOnItemClickListener(this);
         mSearchBar.setAdapter(new SettingsSearchFilterAdapter(this));
 
@@ -322,15 +328,14 @@ public class Settings extends PreferenceActivity
             });
         }
 
-        mActionBar = getActionBar();
-        if (mActionBar != null) {
-            // Override up navigation for multi-pane, since we handle it in the fragment breadcrumbs
-            if (onIsMultiPane()) {
-                mActionBar.setDisplayHomeAsUpEnabled(false);
-                mActionBar.setHomeButtonEnabled(false);
-            }
-            mActionBar.setDisplayShowCustomEnabled(true);
+        // Override up navigation for multi-pane, since we handle it in the fragment breadcrumbs
+        if (onIsMultiPane()) {
+            getActionBar().setDisplayHomeAsUpEnabled(false);
+            getActionBar().setHomeButtonEnabled(false);
         }
+
+        mActionBar = getActionBar();
+        mActionBar.setDisplayShowCustomEnabled(true);
     }
 
     @Override
@@ -410,13 +415,6 @@ public class Settings extends PreferenceActivity
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mSearchBar.clearFocus();
-        mSearchItem.collapseActionView();
-    }
-
-    @Override
     public void onDestroy() {
         super.onDestroy();
         if (mListeningToAccountUpdates) {
@@ -476,18 +474,18 @@ public class Settings extends PreferenceActivity
         KeyboardLayoutPickerFragment.class.getName(),
         BlacklistSettings.class.getName(),
         ApnSettings.class.getName(),
-	QuietHours.class.getName(),
+	    QuietHours.class.getName(),
         QuickSettingsTiles.class.getName(),
-	BatteryIconStyle.class.getName(),
+	    BatteryIconStyle.class.getName(),
         DisplayRotation.class.getName(),
         HomeSettings.class.getName(),
         ButtonSettings.class.getName(),
         ProfilesSettings.class.getName(),
         PolicyNativeFragment.class.getName(),
         com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
-	ThemeSettings.class.getName(),
+	    ThemeSettings.class.getName(),
         ShakeEvents.class.getName(),
-	com.android.settings.wifi.WifiApSettings.class.getName(),
+	    com.android.settings.wifi.WifiApSettings.class.getName(),
         com.android.settings.carbon.superuser.PolicyNativeFragment.class.getName()	
     };
 
@@ -1018,10 +1016,8 @@ public class Settings extends PreferenceActivity
         private final WifiEnabler mWifiEnabler;
         private final BluetoothEnabler mBluetoothEnabler;
         private final ProfileEnabler mProfileEnabler;
-        private final TRDSEnabler mTRDSEnabler;
         private final LocationEnabler mLocationEnabler;
         private final VoiceWakeupEnabler mVoiceWakeupEnabler;
-        private final LocationEnabler mLocationEnabler;
         public static ThemeEnabler mThemeEnabler;
         private AuthenticatorHelper mAuthHelper;
         private DevicePolicyManager mDevicePolicyManager;
@@ -1047,7 +1043,6 @@ public class Settings extends PreferenceActivity
                 return HEADER_TYPE_SWITCH;
             } else if (header.id == R.id.wifi_settings
 		    || header.id == R.id.bluetooth_settings
-		    || header.id == R.id.trds_settings
                     || header.id == R.id.profiles_settings
                     || header.id == R.id.voice_wakeup_settings
                     || header.id == R.id.location_settings
@@ -1174,9 +1169,6 @@ public class Settings extends PreferenceActivity
                         mBluetoothEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.profiles_settings) {
                         mProfileEnabler.setSwitch(holder.switch_);
-                    } else if (header.id == R.id.trds_settings) {
-                        mTRDSSwitch = (Switch) view.findViewById(R.id.switchWidget);
-                        mTRDSEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.location_settings) {
                         mLocationEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.voice_wakeup_settings) {
@@ -1230,10 +1222,8 @@ public class Settings extends PreferenceActivity
                     String accType = header.extras.getString(
                             ManageAccountsSettings.KEY_ACCOUNT_TYPE);
                     Drawable icon = mAuthHelper.getDrawableForType(getContext(), accType);
-                    updateIconLayout(holder, true);
-                    holder.icon.setImageDrawable(icon);
+                    setHeaderIcon(holder, icon);
                 } else {
-                    updateIconLayout(holder, false);
                     holder.icon.setImageResource(header.iconRes);
                 }
                 holder.title.setText(header.getTitle(getContext().getResources()));
@@ -1246,23 +1236,19 @@ public class Settings extends PreferenceActivity
                 }
             }
 
-        private void updateIconLayout(HeaderViewHolder holder, boolean forceDefaultSize) {
+        private void setHeaderIcon(HeaderViewHolder holder, Drawable icon) {
             ViewGroup.LayoutParams lp = holder.icon.getLayoutParams();
-            if (forceDefaultSize) {
-                lp.width = getContext().getResources().getDimensionPixelSize(
-                        R.dimen.header_icon_width);
-            } else {
-                lp.width = ViewGroup.LayoutParams.WRAP_CONTENT;
-            }
+            lp.width = getContext().getResources().getDimensionPixelSize(
+                    R.dimen.header_icon_width);
             lp.height = lp.width;
             holder.icon.setLayoutParams(lp);
+            holder.icon.setImageDrawable(icon);
         }
 
         public void resume() {
             mWifiEnabler.resume();
             mBluetoothEnabler.resume();
             mProfileEnabler.resume();
-            mTRDSEnabler.resume();
             mLocationEnabler.resume();
             mVoiceWakeupEnabler.resume();
             mThemeEnabler.resume();
@@ -1272,7 +1258,6 @@ public class Settings extends PreferenceActivity
             mWifiEnabler.pause();
             mBluetoothEnabler.pause();
             mProfileEnabler.pause();
-            mTRDSEnabler.pause();
             mLocationEnabler.pause();
             mVoiceWakeupEnabler.pause();
             mThemeEnabler.resume();
@@ -1286,6 +1271,19 @@ public class Settings extends PreferenceActivity
             revert = true;
         }
 
+        // a temp hack while we prepare to switch
+        // to the new theme chooser.
+        if (header.id == R.id.theme_settings) {
+            try {
+                Intent intent = new Intent();
+                intent.setClassName("com.tmobile.themechooser", "com.tmobile.themechooser.ThemeChooser");
+                startActivity(intent);
+                return;
+            } catch(ActivityNotFoundException e) {
+                 // Do nothing, we will launch the submenu
+            }
+        }
+
         super.onHeaderClick(header, position);
 
         if (revert && mLastHeader != null) {
@@ -1297,7 +1295,9 @@ public class Settings extends PreferenceActivity
 
     @Override
     public boolean onPreferenceStartFragment(PreferenceFragment caller, Preference pref) {
-        mSearchItem.collapseActionView();
+        if (mSearchItem != null) {
+            mSearchItem.collapseActionView();
+        }
         // Override the fragment title for Wallpaper settings
         int titleRes = pref.getTitleRes();
         if (pref.getFragment().equals(OwnerInfoSettings.class.getName())
@@ -1340,6 +1340,13 @@ public class Settings extends PreferenceActivity
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
+
+        if (mSearchBar != null) {
+            mSearchBar.clearFocus();
+        }
+        if (mSearchItem != null) {
+            mSearchItem.collapseActionView();
+        }
 
         if (newConfig.uiThemeMode != mCurrentState && HeaderAdapter.mThemeEnabler != null) {
             mCurrentState = newConfig.uiThemeMode;
