6bb7809dfb9c75afd031b7d9fd9c60e6281b5e80
Clark Scheff <clark@cyngn.com>
2014-10-28 15:56:38 -0700

    Launch theme chooser that supports cyngn.intent.category.APP_THEMES
    If we receive an ActivityNotFound exception we will simply show
    the theme settings fragment.
    
    Change-Id: I38bf6857125cdeda310e3bb5614fd6adaba8265f
    
    Conflicts:
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -157,6 +157,8 @@ public class Settings extends PreferenceActivity
 
     private static final String VOICE_WAKEUP_PACKAGE_NAME = "com.cyanogenmod.voicewakeup";
 
+    private static final String THEME_CHOOSER_CATEGORY = "cyngn.intent.category.APP_THEMES";
+
     static final int DIALOG_ONLY_ONE_HOME = 1;
 
     private static boolean sShowNoHomeNotice = false;
@@ -1271,16 +1273,16 @@ public class Settings extends PreferenceActivity
             revert = true;
         }
 
-        // a temp hack while we prepare to switch
-        // to the new theme chooser.
+        // Launch the theme chooser if it supports the cyngn.intent.category.APP_THEMES category.
         if (header.id == R.id.theme_settings) {
+            Intent intent = new Intent(Intent.ACTION_MAIN)
+                    .addCategory(THEME_CHOOSER_CATEGORY)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             try {
-                Intent intent = new Intent();
-                intent.setClassName("com.tmobile.themechooser", "com.tmobile.themechooser.ThemeChooser");
                 startActivity(intent);
                 return;
-            } catch(ActivityNotFoundException e) {
-                 // Do nothing, we will launch the submenu
+            } catch (ActivityNotFoundException e) {
+                // do nothing so the theme settings to be displayed
             }
         }
 
